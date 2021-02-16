b5fd2b270da06f51d82276994bb465ff5ff9b58d
Mohamed ElSayed <m.elsayed4420@gmail.com>
2021-02-16 17:15:03 +0200

    removing files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moe/Elegant86/Elegant86
-# Build directory: /home/moe/Elegant86/Elegant86
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
